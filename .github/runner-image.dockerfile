3f2b3129c14ebbad21a489777d1fb4ad618a031d
Philip Vejre <philip@cyber-crypt.com>
2021-10-20 14:55:48 +0200

    Update makefile+pipeline to work with new E2E test functionality

--- .github/runner-image.dockerfile
+++ .github/runner-image.dockerfile
@@ -48,7 +48,7 @@ RUN curl -fsSL https://get.docker.com -o get-docker.sh && \
   rm -rf /usr/libexec/docker
 
 # Docker compose
-RUN curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/bin/docker-compose && \
+RUN wget -O /usr/bin/docker-compose https://github.com/docker/compose/releases/download/v2.0.1/docker-compose-linux-x86_64 && \
   chmod +x /usr/bin/docker-compose
 
 # kubectl
---
+++ encryption-service/scripts/get-e2e-user.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# Copyright 2021 CYBERCRYPT
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+set -euo pipefail
+
+source ./scripts/dev-env
+
+USER_INFO=""
+
+if [[ "${1}" == "local" ]]; then
+  USER_INFO=$(./encryption-service create-user m 2> /dev/null)
+elif [[ "${1}" == "docker" ]]; then
+  USER_INFO=$(docker exec encryption-service-encryption-service-1 /encryption-service create-user m 2> /dev/null)
+elif [[ "${1}" == "kubernetes" ]]; then
+  USER_INFO=$(kubectl -n encryptonize exec deployment/encryptonize-deployment -- /encryption-service create-user m 2> /dev/null)
+else
+  echo "Unknown argument '${1}'"
+  exit 1
+fi
+
+echo "export E2E_TEST_UID=$(echo $USER_INFO | jq -r ".user_id")"
+echo "export E2E_TEST_PASS=$(echo $USER_INFO | jq -r ".password")"
