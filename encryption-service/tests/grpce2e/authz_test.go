a5a7b75f8937cde896f82c381e53ceb8e85fd2f9
Nela Kaczmarek <nela@cyber-crypt.com>
2021-11-12 10:11:33 +0100

    Add E2E authentication and authorization tests

--- encryption-service/tests/grpce2e/authz_test.go
+++ encryption-service/tests/grpce2e/authz_test.go
@@ -18,11 +18,13 @@
 package grpce2e
 
 import (
+	"encryption-service/common"
+
 	"testing"
 )
 
 // Test that unauthorized users cannot perform actions on objects
-func TestUnauthorizedAccess(t *testing.T) {
+func TestUnauthorizedAccessToObject(t *testing.T) {
 	client, err := NewClient(endpoint, https)
 	failOnError("Could not create client", err, t)
 	defer closeClient(client, t)
@@ -74,4 +76,156 @@ func TestUnauthorizedAccess(t *testing.T) {
 
 	_, err = client.RemovePermission(oidStored, uid)
 	failOnSuccess("Unauthorized user removed permission", err, t)
+}
+
+func TestUnauthorizedToCreate(t *testing.T) {
+	client, err := NewClient(endpoint, https)
+	failOnError("Could not create client", err, t)
+	defer closeClient(client, t)
+
+	_, err = client.LoginUser(uid, pwd)
+	failOnError("Could not log in user", err, t)
+
+	var scopes = []common.UserScope{
+		common.UserScope_READ,
+		common.UserScope_INDEX,
+		common.UserScope_OBJECTPERMISSIONS,
+		common.UserScope_USERMANAGEMENT,
+		common.UserScope_UPDATE,
+		common.UserScope_DELETE,
+	}
+
+	createUserResponse, err := client.CreateUser(scopes)
+	failOnError("Create user request failed", err, t)
+
+	_, err = client.LoginUser(createUserResponse.UserId, createUserResponse.Password)
+	failOnError("Could not log in user", err, t)
+
+	_, err = client.Store([]byte("foo"), []byte("bar"))
+	failOnSuccess("User should not be able to store object without CREATE scope", err, t)
+}
+
+func TestUnauthorizedToGetPermissions(t *testing.T) {
+	client, err := NewClient(endpoint, https)
+	failOnError("Could not create client", err, t)
+	defer closeClient(client, t)
+
+	_, err = client.LoginUser(uid, pwd)
+	failOnError("Could not log in user", err, t)
+
+	var scopes = []common.UserScope{
+		common.UserScope_READ,
+		common.UserScope_CREATE,
+		common.UserScope_OBJECTPERMISSIONS,
+		common.UserScope_USERMANAGEMENT,
+		common.UserScope_UPDATE,
+		common.UserScope_DELETE,
+	}
+
+	createUserResponse, err := client.CreateUser(scopes)
+	failOnError("Create user request failed", err, t)
+
+	_, err = client.LoginUser(createUserResponse.UserId, createUserResponse.Password)
+	failOnError("Could not log in user", err, t)
+
+	storeResponse, err := client.Store([]byte("foo"), []byte("bar"))
+	failOnError("Store operation failed", err, t)
+	oidStored := storeResponse.ObjectId
+
+	_, err = client.GetPermissions(oidStored)
+	failOnSuccess("User should not be able to get permission without INDEX scope", err, t)
+}
+
+func TestUnauthorizedToManagePermissions(t *testing.T) {
+	client, err := NewClient(endpoint, https)
+	failOnError("Could not create client", err, t)
+	defer closeClient(client, t)
+
+	_, err = client.LoginUser(uid, pwd)
+	failOnError("Could not log in user", err, t)
+
+	var scopes = []common.UserScope{
+		common.UserScope_READ,
+		common.UserScope_CREATE,
+		common.UserScope_INDEX,
+		common.UserScope_USERMANAGEMENT,
+		common.UserScope_UPDATE,
+		common.UserScope_DELETE,
+	}
+
+	createUserResponse, err := client.CreateUser(scopes)
+	failOnError("Create user request failed", err, t)
+
+	_, err = client.LoginUser(createUserResponse.UserId, createUserResponse.Password)
+	failOnError("Could not log in user", err, t)
+
+	storeResponse, err := client.Store([]byte("foo"), []byte("bar"))
+	failOnError("Store operation failed", err, t)
+	oidStored := storeResponse.ObjectId
+
+	_, err = client.AddPermission(oidStored, uid)
+	failOnSuccess("User should not be able to add permission without OBJECTPERMISSIONS scope", err, t)
+
+	_, err = client.RemovePermission(oidStored, uid)
+	failOnSuccess("User should not be able to remove permission without OBJECTPERMISSIONS scope", err, t)
+}
+
+func TestUnauthorizedToManageUsers(t *testing.T) {
+	client, err := NewClient(endpoint, https)
+	failOnError("Could not create client", err, t)
+	defer closeClient(client, t)
+
+	_, err = client.LoginUser(uid, pwd)
+	failOnError("Could not log in user", err, t)
+
+	var scopes = []common.UserScope{
+		common.UserScope_READ,
+		common.UserScope_CREATE,
+		common.UserScope_INDEX,
+		common.UserScope_OBJECTPERMISSIONS,
+		common.UserScope_UPDATE,
+		common.UserScope_DELETE,
+	}
+
+	createUserResponse, err := client.CreateUser(scopes)
+	failOnError("Create user request failed", err, t)
+
+	_, err = client.LoginUser(createUserResponse.UserId, createUserResponse.Password)
+	failOnError("Could not log in user", err, t)
+
+	_, err = client.CreateUser(protoUserScopes)
+	failOnSuccess("User should not be able to create user without USERMANAGEMENT scope", err, t)
+}
+
+func TestUnauthorizedToUpdateAndDelete(t *testing.T) {
+	client, err := NewClient(endpoint, https)
+	failOnError("Could not create client", err, t)
+	defer closeClient(client, t)
+
+	_, err = client.LoginUser(uid, pwd)
+	failOnError("Could not log in user", err, t)
+
+	var scopes = []common.UserScope{
+		common.UserScope_READ,
+		common.UserScope_CREATE,
+		common.UserScope_INDEX,
+		common.UserScope_OBJECTPERMISSIONS,
+		common.UserScope_USERMANAGEMENT,
+	}
+
+	createUserResponse, err := client.CreateUser(scopes)
+	failOnError("Create user request failed", err, t)
+
+	_, err = client.LoginUser(createUserResponse.UserId, createUserResponse.Password)
+	failOnError("Could not log in user", err, t)
+
+	storeResponse, err := client.Store([]byte("foo"), []byte("bar"))
+	failOnError("Store operation failed", err, t)
+	oidStored := storeResponse.ObjectId
+
+	_, err = client.Update(oidStored, []byte("new_foo"), []byte("new_bar"))
+	failOnSuccess("User should not be able to delete object without UPDATE scope", err, t)
+
+	_, err = client.Delete(oidStored)
+	failOnSuccess("User should not be able to delete object without DELETE scope", err, t)
 }
