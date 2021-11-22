fdc977a070aa1f659f291b175a9b07c7e1101dc4
Philip Vejre <philip@cyber-crypt.com>
2021-11-22 16:08:09 +0100

    Reorganize encryptonize cluster deployment scripts

---
+++ kubernetes/scripts/bootstrap-encryptonize.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+# Copyright 2021 CYBERCRYPT
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+set -euo pipefail
+
+BLUE_ON="\u1b[1;34m"
+COLOR_OFF="\u1b[m"
+
+ROOT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && cd .. && pwd )"
+OUT_DIR="${ROOT_DIR}/encryptonize/bootstrap/config"
+
+mkdir -p "${OUT_DIR}"
+
+# Get object cluster secrets
+gcloud container clusters get-credentials $OBJ_CLUSTER --zone $ZONE --project $PROJECT
+
+echo -e "${BLUE_ON}[+] Fetching object storage secrets${COLOR_OFF}"
+kubectl -n rook-ceph get secret bucket-claim -o jsonpath="{.data['AWS_ACCESS_KEY_ID']}" | base64 -d > "${OUT_DIR}/object_storage_id"
+kubectl -n rook-ceph get secret bucket-claim -o jsonpath="{.data['AWS_SECRET_ACCESS_KEY']}" | base64 -d > "${OUT_DIR}/object_storage_key"
+kubectl -n rook-ceph get secret object-certs -o jsonpath="{.data.object_storage\.crt}" | base64 -d > "${OUT_DIR}/object_storage.crt"
+
+# Bootstrap encryptonize
+gcloud container clusters get-credentials $ENC_CLUSTER --zone $ZONE --project $PROJECT
+
+echo -e "${BLUE_ON}[+] Bootstrapping encryptonize cluster${COLOR_OFF}"
+kubectl apply -k "${ROOT_DIR}/encryptonize/bootstrap"
